c017fb97a484d483b3d8c257c55977aaad1bad32
Kishan Joshi <robokishan.blogspot@gmail.com>
2021-11-04 17:10:17 +0530

    Add : mongoRepository method for getting authenticated answer list

--- controllers/typeGraphqlResolvers/FormAnsResolver.ts
+++ controllers/typeGraphqlResolvers/FormAnsResolver.ts
@@ -1,9 +1,9 @@
 import { Authorized, Ctx, Query, Resolver } from "type-graphql";
+import { getMongoRepository } from "typeorm";
 import { formanswers } from "../../models/typeormEnt/v1/FormAns";
-var Answer = require("../../models/mongo/v1/FormAns");
-var Asset = require("../../models/mongo/v1/Form");
-
+import { Forms } from "../../models/typeormEnt/v1/Forms";
 import { Context } from "../../types/Context";
+
 @Resolver()
 export class FormAnsResolver {
   @Authorized()
@@ -11,24 +11,55 @@ export class FormAnsResolver {
   async answers(@Ctx() { req, res }: Context) {
     const userId = req.userId;
 
+    // #1 : mongoose method
+    // try {
+    //   const assets = await Asset.find({ user_id: userId }).lean().exec();
+    //   const ids = assets.map((asset) => asset._id);
+    //   let answers = await Answer.find({ form_id: { $in: ids } })
+    //     .lean()
+    //     .exec();
+    //   for (let answer of answers) {
+    //     let asset = await Asset.findOne({
+    //       user_id: userId,
+    //       _id: answer.form_id,
+    //     }).exec();
+    //     answer["title"] = asset["title"];
+    //     answer["description"] = asset["description"];
+    //   }
+    //   console.log(answers);
+    //   return answers;
+    // } catch (err) {
+    //   console.error("Form Answer error", err);
+    // }
+
+    // #2 using mongorepository
+    // found getMongomanager vs getMongoRepository
+
     try {
-      const assets = await Asset.find({ user_id: userId }).lean().exec();
-      const ids = assets.map((asset) => asset._id);
-      let answers = await Answer.find({ form_id: { $in: ids } })
-        .lean()
-        .exec();
-      for (let answer of answers) {
-        let asset = await Asset.findOne({
-          user_id: userId,
-          _id: answer.form_id,
-        }).exec();
-        answer["title"] = asset["title"];
-        answer["description"] = asset["description"];
-      }
-      console.log(answers);
+      let assetManager = getMongoRepository(Forms);
+      let answerManager = getMongoRepository(formanswers);
+
+      const assets = await assetManager
+        .aggregate([
+          {
+            $match: { user_id: userId },
+          },
+          {
+            $addFields: {
+              id: { $toString: "$_id" },
+            },
+          },
+        ])
+        .toArray();
+
+      const ids = assets.map((asset) => asset.id);
+      let answers = await answerManager.find({
+        where: { form_id: { $in: ids } },
+      });
+
       return answers;
-    } catch (err) {
-      console.error("Form Answer error", err);
+    } catch (error) {
+      console.log("Error", error);
     }
   }
 }
